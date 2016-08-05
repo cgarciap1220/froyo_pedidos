cfd0149736c15c71f7afaf8a3881c824da98772b
cgarciap1220 <cgarciap1220@gmail.com>
2016-08-04 18:22:39 -0600

    creacion de modelos

---
+++ application/models/Ruta_model.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Ruta_model
+ *
+ * @author Cindy
+ */
+class Ruta_model {
+    //put your code here
+}
